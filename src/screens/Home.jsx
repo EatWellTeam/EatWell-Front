52a15bc3370958ab21c21fa27664f00432655846
yarin1849 <yarin1849@gmail.com>
2024-07-18 19:31:24 +0300

    adding features

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, SafeAreaView, Image, Platform, StyleSheet, Button, Alert } from 'react-native';
 
+
 export default function HomeScreen() {
     return (
       <SafeAreaView style={styles.container}>
-      <Text>Home Screen</Text>
-      <Image source={require('../../assets/eat-well-logo.png')}/>
-      <Button title="click me" onPress={()=>Alert.alert("you have clicked btn", "there is no more to do")}/>
+        <Text>WELCOME</Text>
+        <Image source={require('../../assets/eat-well-logo.png')} />
+        <Button
+          title="Log in"
+          onPress={() => Alert.alert("you have clicked btn", "there is no more to do")}
+          color="green"
+        />
+        <View style={{ marginTop: 10 }}></View>
+        <Button 
+          title="Sign up"
+          onPress={() => Alert.alert("you have clicked btn", "there is no more to do")}
+          color="green"
+        />
+
         <StatusBar style="auto" />
-    </SafeAreaView>
+      </SafeAreaView>
     );
   }
 
