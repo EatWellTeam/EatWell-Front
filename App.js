eb0fd09c24403f880b052b5a2695157e0fe3dfec
Arielle Ben Ovadia <arielle.benovadia2@gmail.com>
2024-07-22 21:18:12 +0300

    pull commit

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -15,7 +13,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={HomeScreen} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={LoginScreen} />
