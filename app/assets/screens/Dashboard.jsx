d7e5a850668db620adf3f2a8a93d19c8d2f3dac3
shlomi90 <shlomikaravani@gmail.com>
2024-08-13 21:12:30 +0300

    s

--- app/assets/screens/Dashboard.jsx
+++ app/assets/screens/Dashboard.jsx
@@ -106,7 +106,7 @@ export default function DashboardScreen() {
         role: 'user',
       };
 
-      const { data } = await axios.post(`${API_URL}/middleware/process`, [payload], {
+      const { data } = await axios.post(`http://10.160.1.46:3000/middleware/process`, [payload], {
         headers: {
           'Content-Type': 'application/json',
         },
